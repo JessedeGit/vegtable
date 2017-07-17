84d7d8652ce251bbf8f8087cb0390f8739e71d9f
Safemaster <Safemaster>
2017-07-18 07:54:16 +0800

    Able to deal with empty product list and give a next sale date.

--- controllers/home.js
+++ controllers/home.js
@@ -10,17 +10,35 @@
 var fs = require('fs');
 // var products = Array.from(JSON.parse(fs.readFileSync('./data/currPrdt.data', 'utf8')).items);
 let products = {};
+let nextSale = 0;
 fs.readFile('./data/currPrdt.data', 'utf8', function (err, data) {
   if (err) throw err;
-  products = Array.from(JSON.parse(data).items);
+  // if (!fs.isEmptySync('./data/currPrdt.data'))
+  
+  if(!isJSON(data)) {
+    console.log('data: ' + data);
+    products = [];
+    nextSale = data.toString();
+  } else
+    products = Array.from(JSON.parse(data).items);
 });
 
 exports.index = (req, res) => {
   res.render('home', {
     title: 'Home',
     products,
+    nextSale,
   });
 };
+
+function isJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
 
 // var fs = require('fs');
 // var async = require('async');
